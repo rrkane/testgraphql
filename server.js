55e685233162f73f4566d0b0b7db273ebb62c720
rrkane <raj.r.kane@protonmail.com>
2020-04-06 16:29:17 -0700

    return author on book query

--- server.js
+++ server.js
@@ -12,13 +12,30 @@ const {
 const books = [
     {id: 1, name: 'Notes from Underground', authorId: 1},
     {id: 2, name: 'The Trial', authorId: 2},
-    {id: 3, name: 'The Brothers Karamazov', authorId: 1}
+    {id: 3, name: 'Brave New World', authorId: 3},
+    {id: 4, name: 'Into the Wild', authorId: 4},
+    {id: 5, name: 'The Picture of Dorian Grey', authorId: 5},
+    {id: 6, name: '1984', authorId: 6},
+    {id: 7, name: 'Animal Farm', authorId: 6}
 ];
 
 const authors = [
     {id: 1, name: 'Dostoyevsky'},
-    {id: 2, name: 'Kafka'}
+    {id: 2, name: 'Kafka'},
+    {id: 3, name: 'Huxley'},
+    {id: 4, name: 'Krakeur'},
+    {id: 5, name: 'Wilde'},
+    {id: 6, name: 'Orwell'}
 ];
+
+const AuthorType = new GraphQLObjectType({
+    name: 'Author',
+    description: 'An author of a book',
+    fields: () => ({
+        id: {type: GraphQLInt},
+        name: {type: GraphQLNonNull(GraphQLString)},
+    })
+});
 
 const BookType = new GraphQLObjectType({
     name: 'Book',
@@ -26,7 +43,15 @@ const BookType = new GraphQLObjectType({
     fields: () => ({
         id: {type: GraphQLInt},
         name: {type: GraphQLNonNull(GraphQLString)},
-        authorId: {type: GraphQLNonNull(GraphQLInt)}
+        authorId: {type: GraphQLNonNull(GraphQLInt)},
+        author: {
+            type: AuthorType,
+            resolve: (book) => {
+                return authors.find(
+                    author => author.id === book.authorId
+                )
+            }
+        }
     })
 });
 
